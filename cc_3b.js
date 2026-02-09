50319e94869cadaac98832998fd6ee4a461306e5
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-08 20:44:39 -0500

    added push, pop and update requirements

--- cc_3b.js
+++ cc_3b.js
@@ -1,8 +1,12 @@
-let inventory = [ // array created
-    ["prt22332"],
-    ["Laptop"],
-    [2059.99],
-    [55]
-]
+let inventory = [["prt22332", "Laptop", 2059.99, 55]] //array created
 
-console.log(`${inventory[0]} | ${inventory[1]} | $${inventory[2]} | Stock:${inventory[3]}`)
+console.log(`${inventory[0][0]} | ${inventory[0][1]} | $${inventory[0][2]} | Stock:${inventory[0][3]}`)
+
+inventory.push(["ret55343", "Keyboard", 54.99, 102]) //push, pop, update
+inventory.pop()
+console.log('Removed Keyboard entry')
+inventory[0][2] = 1549.99
+inventory[0][3] = 80
+console.log(inventory)
+
+
