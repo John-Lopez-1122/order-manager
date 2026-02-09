672b65516d710e3de77fa8628a472713e021a2e8
John-Lopez-1122 <jlopez354@usf.edu>
2026-02-08 20:31:16 -0500

    Fixed multiple errors (src mispelling, incorrect array separation)

---
+++ cc_3b.js
@@ -0,0 +1,8 @@
+let inventory = [ // array created
+    ["prt22332"],
+    ["Laptop"],
+    [2059.99],
+    [55]
+]
+
+console.log(`${inventory[0]} | ${inventory[1]} | $${inventory[2]} | Stock:${inventory[3]}`)
